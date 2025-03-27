f6ef2942727511e46655b27cc1b8942955b00cc1
Brian Chen <bzchen93@gmail.com>
2025-03-27 20:21:47 +1100

    movement system working and tested

--- src/components/direction.zig
+++ src/components/direction.zig
@@ -3,10 +3,10 @@ const std = @import("std");
 pub const Direction = struct {
     allocator: std.mem.Allocator,
     /// From 0 - 359 where 0 is top dead center
-    direction: *u9,
+    direction: *f32,
 
-    pub fn init(allocator: std.mem.Allocator, initialDirection: u9) !Direction {
-        const direction = try allocator.create(u9);
+    pub fn init(allocator: std.mem.Allocator, initialDirection: f32) !Direction {
+        const direction = try allocator.create(f32);
         direction.* = initialDirection;
 
         return Direction{
@@ -19,38 +19,40 @@ pub const Direction = struct {
         self.allocator.destroy(self.direction);
     }
 
-    pub fn updateDirection(self: *const Direction, newDirection: u9) !void {
-        self.direction.* = newDirection % 359;
+    pub fn updateDirection(self: *const Direction, newDirection: f32) !void {
+        self.direction.* = @mod(newDirection, 360);
     }
 };
 
 test "Direction - creation" {
     const allocator = std.testing.allocator;
 
-    const direction = try Direction.init(allocator, 0);
+    const direction = try Direction.init(allocator, 0.0);
     defer direction.deinit();
 
-    try std.testing.expect(direction.direction.* == 0);
+    try std.testing.expect(direction.direction.* == 0.0);
 }
 
 test "Direction - change direction" {
     const allocator = std.testing.allocator;
 
-    const direction = try Direction.init(allocator, 0);
+    const direction = try Direction.init(allocator, 0.0);
     defer direction.deinit();
 
-    try direction.updateDirection(240);
+    try direction.updateDirection(240.0);
 
-    try std.testing.expect(direction.direction.* == 240);
+    try std.testing.expect(direction.direction.* == 240.0);
 }
 
 test "Direction - change direction overflow" {
     const allocator = std.testing.allocator;
 
-    const direction = try Direction.init(allocator, 0);
+    const direction = try Direction.init(allocator, 0.0);
     defer direction.deinit();
 
-    try direction.updateDirection(480);
+    try direction.updateDirection(480.0);
+    try std.testing.expect(direction.direction.* == 120.0);
 
-    try std.testing.expect(direction.direction.* == 121);
+    try direction.updateDirection(360.0);
+    try std.testing.expect(direction.direction.* == 0.0);
 }
--- src/systems/movement.zig
+++ src/systems/movement.zig
@@ -8,29 +8,53 @@ const ActOn = struct {
     direction: components.Direction,
 };
 
+fn distanceBySpeed(speedPerSecond: f32, diff: i64) f32 {
+    return speedPerSecond * (@as(f32, @floatFromInt(diff)) / 1000.0);
+}
+
+fn updatePosition(x: f64, y: f32, degrees: f32, distance: f32) struct { x: f32, y: f32 } {
+    const radians = degrees * std.math.pi / 180;
+    const delta_x = distance * std.math.sin(radians);
+    const delta_y = distance * std.math.cos(radians);
+    return .{
+        .x = @as(f32, @floatCast(x + delta_x)),
+        .y = @as(f32, @floatCast(y + delta_y)),
+    };
+}
+
 pub const Movement = struct {
     allocator: std.mem.Allocator,
-    act_on: std.ArrayListUnmanaged(ActOn),
+    entities: std.ArrayListUnmanaged(ActOn),
 
     pub fn init(allocator: std.mem.Allocator) Movement {
         return Movement{
             .allocator = allocator,
-            .act_on = std.ArrayListUnmanaged(ActOn).empty,
+            .entities = std.ArrayListUnmanaged(ActOn).empty,
         };
     }
 
     pub fn deinit(self: *Movement) void {
-        self.act_on.deinit(self.allocator);
+        self.entities.deinit(self.allocator);
     }
 
     pub fn addEntity(self: *Movement, entity: ActOn) !void {
-        try self.act_on.append(self.allocator, entity);
+        try self.entities.append(self.allocator, entity);
     }
 
     /// Runs every tick
-    pub fn update(delta: i32) void {
-        _ = delta;
-        // TODO:
+    pub fn update(self: *const Movement, delta: i64) void {
+        for (self.entities.items) |entity| {
+            if (!entity.moveable.moving.*) {
+                continue;
+            }
+            const x = entity.position.position.x;
+            const y = entity.position.position.y;
+            const degrees = entity.direction.direction;
+            const distance = distanceBySpeed(entity.moveable.speed.*, delta);
+            const new_position = updatePosition(x, y, degrees.*, distance);
+
+            entity.position.position.* = rl.Vector2.init(new_position.x, new_position.y);
+        }
     }
 };
 
@@ -60,5 +84,136 @@ test "Movement - add entity" {
         .position = positionComp,
     });
 
-    try std.testing.expect(movement.act_on.items.len == 1);
+    try std.testing.expect(movement.entities.items.len == 1);
+}
+
+test "Movement - move entities basic" {
+    const allocator = std.testing.allocator;
+
+    var movement = Movement.init(allocator);
+    defer movement.deinit();
+
+    const moveableComp = try components.Moveable.init(allocator, 5.0);
+    defer moveableComp.deinit();
+    moveableComp.startMoving();
+    const positionComp = try components.Position.init(allocator, 0, 0);
+    defer positionComp.deinit();
+    const directionComp = try components.Direction.init(allocator, 90.0);
+    defer directionComp.deinit();
+
+    try movement.addEntity(.{
+        .moveable = moveableComp,
+        .direction = directionComp,
+        .position = positionComp,
+    });
+
+    movement.update(100);
+
+    const final_position = movement.entities.items[0].position.position;
+    try std.testing.expect(final_position.x == 0.5);
+    try std.testing.expectApproxEqAbs(0, final_position.y, 1e-6);
+
+    movement.update(100);
+    movement.update(100);
+    movement.update(100);
+
+    try std.testing.expect(final_position.x == 2);
+    try std.testing.expectApproxEqAbs(0, final_position.y, 1e-6);
+}
+
+test "Movement - move entities negative" {
+    const allocator = std.testing.allocator;
+
+    var movement = Movement.init(allocator);
+    defer movement.deinit();
+
+    const moveableComp = try components.Moveable.init(allocator, 5.0);
+    defer moveableComp.deinit();
+    moveableComp.startMoving();
+    const positionComp = try components.Position.init(allocator, 0, 0);
+    defer positionComp.deinit();
+    const directionComp = try components.Direction.init(allocator, 240.0);
+    defer directionComp.deinit();
+
+    try movement.addEntity(.{
+        .moveable = moveableComp,
+        .direction = directionComp,
+        .position = positionComp,
+    });
+
+    movement.update(100);
+
+    const final_position = movement.entities.items[0].position.position;
+    try std.testing.expectApproxEqAbs(-0.43, final_position.x, 1e-2);
+    try std.testing.expectApproxEqAbs(-0.25, final_position.y, 1e-2);
+}
+
+test "Movement - does not move entity if not moving" {
+    const allocator = std.testing.allocator;
+
+    var movement = Movement.init(allocator);
+    defer movement.deinit();
+
+    const moveableComp = try components.Moveable.init(allocator, 5.0);
+    defer moveableComp.deinit();
+    const positionComp = try components.Position.init(allocator, 0, 0);
+    defer positionComp.deinit();
+    const directionComp = try components.Direction.init(allocator, 240.0);
+    defer directionComp.deinit();
+
+    try movement.addEntity(.{
+        .moveable = moveableComp,
+        .direction = directionComp,
+        .position = positionComp,
+    });
+
+    movement.update(100);
+
+    const final_position = movement.entities.items[0].position.position;
+    try std.testing.expect(final_position.x == 0);
+    try std.testing.expect(final_position.y == 0);
+}
+
+test "Movement - moves only entities that are moving" {
+    const allocator = std.testing.allocator;
+
+    var movement = Movement.init(allocator);
+    defer movement.deinit();
+
+    const moveableComp = try components.Moveable.init(allocator, 5.0);
+    defer moveableComp.deinit();
+    const positionComp = try components.Position.init(allocator, 0, 0);
+    defer positionComp.deinit();
+    const directionComp = try components.Direction.init(allocator, 240.0);
+    defer directionComp.deinit();
+
+    try movement.addEntity(.{
+        .moveable = moveableComp,
+        .direction = directionComp,
+        .position = positionComp,
+    });
+
+    const moveableComp2 = try components.Moveable.init(allocator, 5.0);
+    defer moveableComp2.deinit();
+    moveableComp2.startMoving();
+    const positionComp2 = try components.Position.init(allocator, 0, 0);
+    defer positionComp2.deinit();
+    const directionComp2 = try components.Direction.init(allocator, 240.0);
+    defer directionComp2.deinit();
+
+    try movement.addEntity(.{
+        .moveable = moveableComp2,
+        .direction = directionComp2,
+        .position = positionComp2,
+    });
+
+    movement.update(100);
+
+    const final_position = movement.entities.items[0].position.position;
+    try std.testing.expect(final_position.x == 0);
+    try std.testing.expect(final_position.y == 0);
+
+    const final_position2 = movement.entities.items[1].position.position;
+    try std.testing.expectApproxEqAbs(-0.43, final_position2.x, 1e-2);
+    try std.testing.expectApproxEqAbs(-0.25, final_position2.y, 1e-2);
 }
